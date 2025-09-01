3d1dcde92483a3cccd6817f6c773b58812c7533d
Dileep Kumar <mdileepkumar578@gmail.com>
2025-09-02 01:24:45 +0530

    Updated project with new changes

--- script.js
+++ script.js
@@ -75,10 +75,18 @@ const typed = new Typed(".multiple-text", {
 const scriptURL =
   "https://script.google.com/macros/s/AKfycbxNzIlq8xg_FnYQFd2EpQfdTv1t7xKmCeMDMYbhlGS2cn7feaYlc3STu_8uYP1wLQLHEg/exec";
 const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg")
+
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   fetch(scriptURL, { method: "POST", body: new FormData(form) })
-    .then((response) => console.log("Success!", response))
+    .then((response) => {
+      msg.innerHTML = "Message Send Successfully"
+      setTimeout(function () {
+        msg.innerHTML = ""
+      }, 5000)
+      form.reset()
+    })
     .catch((error) => console.error("Error!", error.message));
 });
